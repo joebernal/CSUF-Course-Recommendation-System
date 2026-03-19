66cbe1a868d0eadc63b16aba1088edbaea453cfe
owinrojas <owin@owinrojas.com>
2026-03-18 17:52:24 -0700

    routes for testing

--- backend/routes/major_routes.py
+++ backend/routes/major_routes.py
@@ -1,3 +1,10 @@
 from flask import Blueprint, jsonify
+from routes.db_operations import query_db
 
 major_bp = Blueprint("major_bp", __name__)
+
+# available majors
+@major_bp.route("/", methods=["GET"])
+def get_majors():
+	majors = query_db("SELECT major_name,major_acronym FROM `majors`;")
+	return jsonify(majors), 200
